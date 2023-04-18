02212e33a34aacdb0c109f0d7ea9c6f8a9bb5fdc
Van Vu <vanvn.vu@gmail.com>
2023-04-18 10:58:14 +0700

    Added Ghost Room

--- room.ts
+++ room.ts
@@ -18,4 +18,12 @@ let matrix = [
   [2, 0, 3, 3],
 ];
 
+let matrix2 = [
+  [0, 1, 1, 2, 6],
+  [0, 5, 0, 0, 0],
+  [2, 0, 1, 3, 7],
+  [2, 0, 3, 0, 7],
+];
+
 console.log(calcPrice(matrix));
+console.log(calcPrice(matrix2));
